6030ff5e63c4323c178c07fa61720dc1f155529a
Rollyne <rollyne@abv.bg>
2017-06-10 23:06:11 +0300

    Add project files.

---
+++ LearningSystem.Data/LearningSystemContext.cs
@@ -0,0 +1,20 @@
+﻿using LearningSystem.Models.EntityModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace LearningSystem.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public class LearningSystemContext : IdentityDbContext<ApplicationUser>
+    {
+        public LearningSystemContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static LearningSystemContext Create()
+        {
+            return new LearningSystemContext();
+        }
+    }
+}
